8abcaf955b5693e8412ac9617d83cc83af754f2c
David <sheppardd1@students.rowan.edu>
2018-10-05 23:14:41 -0400

    minor changes

--- Hardware PWM/MSP430G2553/main.c
+++ Hardware PWM/MSP430G2553/main.c
@@ -1,12 +1,175 @@
-#include <msp430.h> 
+/* --COPYRIGHT--,BSD_EX
+ * Copyright (c) 2012, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************
+ *
+ *                       MSP430 CODE EXAMPLE DISCLAIMER
+ *
+ * MSP430 code examples are self-contained low-level programs that typically
+ * demonstrate a single peripheral function or device feature in a highly
+ * concise manner. For this the code may rely on the device's power-on default
+ * register values and settings such as the clock configuration and care must
+ * be taken when combining code from several examples to avoid potential side
+ * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
+ * for an API functional library-approach to peripheral configuration.
+ *
+ * --/COPYRIGHT--*/
+//******************************************************************************
+//  MSP430G2xx3 Demo - Timer_A, Toggle P1.0, CCR0 Cont. Mode ISR, DCO SMCLK
+//
+//  Description: Toggle P1.0 using software and TA_0 ISR. Toggles every
+//  50000 SMCLK cycles. SMCLK provides clock source for TACLK.
+//  During the TA_0 ISR, P1.0 is toggled and 50000 clock cycles are added to
+//  CCR0. TA_0 ISR is triggered every 50000 cycles. CPU is normally off and
+//  used only during TA_ISR.
+//  ACLK = n/a, MCLK = SMCLK = TACLK = default DCO
+//
+//           MSP430G2xx3
+//         ---------------
+//     /|\|            XIN|-
+//      | |               |
+//      --|RST        XOUT|-
+//        |               |
+//        |           P1.0|-->LED
+//
+//  D. Dang
+//  Texas Instruments Inc.
+//  December 2010
+//   Built with CCS Version 4.2.0 and IAR Embedded Workbench Version: 5.10
+//******************************************************************************
+/*
 
+//Hardware PWM MSP430G2553
+//Created: 5 October 2018
+//By: David Sheppard
 
-/**
- * main.c
- */
-int main(void)
+#include <msp430G2553.h>
+
+#define LED0 BIT0   //defining LED0 as BIT0
+#define LED1 BIT6   //defining LED1 as BIT6
+#define BUTTON BIT3 //defining BUTTON as BIT3
+
+void main(void)
+
+/*
+ * Concept: Button is debounced by disenabling interrupts from the button for a very short time period
+ * Implementation: When button pressed, start timer and disenable interrupts.
+ * When timer rolls over, reenable interrupts so we can check button again.
+ * LED0 starts out off and LED1 starts out on. They toggle when button is pressed and released
+ * LED0: off when button not pressed, on when pressed. LED1 is opposite.
+ * */
+/*
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  BCSCTL3 = LFXT1S_2;                       //interfaces with crystal (needed for clock to work)
+  P1DIR |= (LED0 + LED1);                   // Set LEDs to be outputs
+  P1OUT &= ~(LED0 + LED1);                  // shut off LED
+  CCTL0 = CCIE;                             // CCR0 interrupt enabled
+  CCR0 = 3277;                              //set capture compare register to about 1/10 of aclk, so button interrupt is disabled for 0.1 s
+  P1IFG &= ~BUTTON;                         // clear the P1.3 interrupt flag
+  P1IE |= BUTTON;                           //enable interrupts from the button
+  TA1CCR0 = 50000;
+  TA1CCR1 = 40000;
+  TA1CTL = TASSEL_2 + MC_2 + ID_3 + TAIE;
+  TA0CCTL1 = OUTMOD_7;
+  P1OUT ^= LED1;                            //I want LED1 and LED0 to always be on at different times, so I'll toggle LED1 now
+
+  __bis_SR_register(LPM0_bits + GIE);       // Enter low power mode w/ interrupt
+
 }
+
+// Timer A0 interrupt service routine
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+{
+
+    TACTL = MC_0;           //stop timer
+    TA0R = 0;               //reset timer reg contents
+    P1IE |= BUTTON;         //reenable port 1 interrupts, so button can be read again
+
+}
+
+
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void Timer1_A (void)
+{
+
+    int Timer0Int = TA1IV;  // since we were having issues with the switch statement
+    switch(Timer0Int)       // Based on the intterupt source
+    {
+    case 2:                 // If CCR1 is triggered, we need to turn LED output to zero for the rest of the cycle
+    {
+        P1OUT &= ~BIT0;     // Turn LED off
+        TA1IV &= ~TA1IV_TACCR1; // Clear the Timer0 interrupt Flag
+    }
+    break;
+    case 10:                // Timer has overflowed, meaning we need to reset our cycle
+    {
+        P1OUT |= BIT0;      // Turn LED on
+        TA1IV &= ~TA1IV_TAIFG; // Clear the Timer0 interrupt Flag
+    }
+    break;
+    }
+
+}
+
+
+// Port 1 interrupt service routine (for button)
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)       //take care of interrupt coming from port 1
+{
+    P1IES ^= BUTTON;                // toggle the interrupt edge,
+    P1OUT ^= (LED1);                  //toggle LEDs
+
+    TACTL = TASSEL_1 + MC_1 + ID_0;     // ACLK, up mode, input divider = 0
+    P1IE &= BUTTON;                     //disable port 1 interrupts (button won't cause interrupt)
+    P1IFG &= ~BUTTON;                   // clear the P1.3 interrupt flag
+}
+
+// Note: the interrupt vector will be called
+// when P1.3 goes from Hi to Low as well as
+// LowtoHigh
+
+*/
+
+#include <msp430g2553.h>
+
+int main(void) {
+    WDTCTL = WDTPW + WDTHOLD; //Disable the Watchdog timer for our convenience.
+    P1DIR |= BIT0; //Set pin 1.2 to the output direction.
+    P1SEL |= BIT0; //Select pin 1.2 as our PWM output.
+    TA0CCR0 = 1000; //Set the period in the Timer A0 Capture/Compare 0 register to 1000 us.
+    TA0CCTL1 = OUTMOD_7;
+    TA0CCR1 = 999; //The period in microseconds that the power is ON. It's half the time, which translates to a 50% duty cycle.
+    TA0CTL = TASSEL_2 + MC_1; //TASSEL_2 selects SMCLK as the clock source, and MC_1 tells it to count up to the value in TA0CCR0.
+    __bis_SR_register(LPM0_bits); //Switch to low power mode 0.
+}
+
